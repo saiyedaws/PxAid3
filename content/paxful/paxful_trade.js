e7a4a5e851b677a7e2a7d68569a9f43211b492d3
saiyedaws <saiyedaws@gmail.com>
2020-10-02 05:57:16 -0400

    update

--- content/paxful/paxful_trade.js
+++ content/paxful/paxful_trade.js
@@ -9,7 +9,7 @@ waitUntilElementExistsViaQuerySelectorAll(
 async function startPaxfulOptimization() 
 {
 	await makeTradeBoxBigger();
-
+    createLeaveFeedbackButton();
 	
      //in paxful_trade_buttons.js
      createButton_typeCode(index);
@@ -41,7 +41,7 @@ async function startPaxfulOptimization()
 
      createButton_onlyAcceptPhysicalGiftCards(index);
      index++;
-     
+
      createButton_angryCustomer(index);
 }
 
@@ -104,7 +104,9 @@ function makeTradeBoxBigger() {
 }
 
 //Helper Functions
-function writeInChat(text, textAreaElement) {
+function writeInChat(text, textAreaElement) 
+{
+
 	var input = textAreaElement;
 	setTimeout(() => {
 		//input.innerHTML = text;
@@ -130,4 +132,69 @@ function focusOnTextBoxAtEndOfTheLine(elementToFocusOn) {
 		range.moveStart("character", varCtlLen);
 		range.select();
 	}
+}
+
+
+function createLeaveFeedbackButton() 
+{
+
+  
+    var leaveFeedbackButton = document.getElementsByClassName("title-container")[0];
+
+    var autoLeaveFeedbackButton = document.createElement("button");
+    autoLeaveFeedbackButton.innerText = "Leave Feedback and Send Thanks";
+    autoLeaveFeedbackButton.type = "button";
+    autoLeaveFeedbackButton['id'] = 'leaveFeedback';
+    leaveFeedbackButton.append(autoLeaveFeedbackButton);
+
+    autoLeaveFeedbackButton.onclick = function () {
+
+        //insert message into tradebox
+        var feedbackMsg = "It was a real pleasure trading with you brother. Just like this trade, every time you open a trade with me, the transaction will go smooth and fast. Thanks for your responsiveness and honesty.";
+        var successfulTradeMsg = 'Thank You it worked Brother. Please leave me a good feedback and add me as Trusted. I will do same!  Brother please trade me if you have anymore giftcards, thank you friend.\r\n\r\nIt was a real pleasure trading with you brother. Just like this trade, every time you open a trade with me, the transaction will go smooth and fast. Thanks for your responsiveness and honesty.\r\n\r\n';
+
+        var chatBoxTextArea = document.querySelector(
+			".px-2.form-control.border-transparent.p-0.pt-1.pt-sm-0.regular-24"
+        );
+        
+      var feedbackMessageBoxArea = document.querySelectorAll('[id="feedback-message"]')[0];
+
+    
+      writeInChat(successfulTradeMsg, chatBoxTextArea);
+      setTimeout(() => {
+        var submitMessageButton = document.querySelectorAll('.d-flex.align-items-center.btn.btn-primary.btn-md')[0];
+        setTimeout(submitMessageButton.click(), 500);
+      }, 200);
+
+      
+      setTimeout(() => 
+      {
+        submitFeedback(feedbackMsg, feedbackMessageBoxArea);
+
+      }, 3000);
+
+    }
+
+}
+
+
+function submitFeedback(text, textAreaElement) 
+{
+    //Credits to this site, 
+    //https://stackoverflow.com/questions/28118268/i-am-trying-to-send-text-messages-on-whatsapp-with-javascript
+
+    var input = textAreaElement;
+    setTimeout(() => {
+        //input.innerHTML = text;
+        input.value = text;
+
+        input.dispatchEvent(new Event('input', { bubbles: true }));
+        focusOnTextBoxAtEndOfTheLine(textAreaElement);
+        var submitFeedbackButton = document.querySelectorAll('[id="feedback-submit"]')[0];
+        submitFeedbackButton.click();
+        setTimeout(submitFeedbackButton.click(), 3000);
+        submitFeedbackButton.click();
+        
+
+    }, 100);
 }
